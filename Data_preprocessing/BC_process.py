79e96e289412af46943ee5104ef1d38c3e2d2986
James McGann <jamesm@ATX-MBP-RQ05P.local>
2022-12-29 20:24:34 -0500

    Update sequence locations on BC_process for v2 chemistry.

--- Data_preprocessing/BC_process.py
+++ Data_preprocessing/BC_process.py
@@ -1,31 +1,35 @@
 ########################################################
 # Process R1 for cellranger-atac pipeline
+# revised 12/29/22 for spatial ATAC v2
 ########################################################
-
+import argparse
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 from gzip import open as gzopen
 
-import argparse
+seq_start = 117
+bc2_start, bc2_end = 22, 30
+bc1_start, bc1_end = 60, 68
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", required=True, help="input file")
-ap.add_argument("-o1", "--output_R1", required=True, help="output file R1")
-ap.add_argument("-o2", "--output_R2", required=True, help="output file R2")
+ap.add_argument("-i", "--input",
+	required=True,
+	help="input file")
+ap.add_argument("-o1", "--output_R1",
+	required=True,
+	help="output file R1")
+ap.add_argument("-o2",
+	"--output_R2",
+	required=True,
+	help="output file R2")
 args = vars(ap.parse_args())
 
 input_file_R1 = args["input"]
 output_file_R1 = args["output_R1"]
 output_file_R2 = args["output_R2"]
 
-seq_start=99
-
-bc2_start=0
-bc2_end=8
-
-bc1_start=38
-bc1_end=46
-
-with gzopen(input_file_R1, "rt") as in_handle_R1, open(output_file_R1, "w") as out_handle_R1, open(output_file_R2, "w") as out_handle_R2:
+with gzopen(input_file_R1, "rt") as in_handle_R1, \
+	open(output_file_R1, "w") as out_handle_R1, \
+	open(output_file_R2, "w") as out_handle_R2:
     for title, seq, qual in FastqGeneralIterator(in_handle_R1):
         new_seq_R1 = seq[seq_start:]
         new_qual_R1 = qual[seq_start:]
