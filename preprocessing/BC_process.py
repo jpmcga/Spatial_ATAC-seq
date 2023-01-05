8f32e8101cc4c373a8b2efdb2594f948643baed4
James McGann <jamesm@ATX-MBP-RQ05P.local>
2023-01-05 11:52:14 -0500

    Test synching R1 and R1 to see if solves output discrepancies.

--- preprocessing/BC_process.py
+++ preprocessing/BC_process.py
@@ -24,17 +24,17 @@ ap.add_argument("-o2",
 args = vars(ap.parse_args())
 
 input_file_R2 = args["input"]
-output_file_R3 = args["output_R3"]
+output_file_R1 = args["output_R1"]
 output_file_R2 = args["output_R2"]
 
 with gzopen(input_file_R2, "rt") as in_handle_R2, \
-	open(output_file_R3, "w") as out_handle_R3, \
+	open(output_file_R1, "w") as out_handle_R1, \
 	open(output_file_R2, "w") as out_handle_R2:
     for title, seq, qual in FastqGeneralIterator(in_handle_R2):
-        new_seq_R3 = seq[seq_start:]
-        new_qual_R3 = qual[seq_start:]
+        new_seq_R1 = seq[seq_start:]
+        new_qual_R1 = qual[seq_start:]
         barcode = seq[bc2_start:bc2_end] + seq[bc1_start:bc1_end] # !!! BC2 + BC1
         new_qual_R2 = qual[bc2_start:bc2_end] + qual[bc1_start:bc1_end]        
-        out_handle_R3.write("@%s\n%s\n+\n%s\n" % (title, new_seq_R3, new_qual_R3))
+        out_handle_R1.write("@%s\n%s\n+\n%s\n" % (title, new_seq_R1, new_qual_R1))
         out_handle_R2.write("@%s\n%s\n+\n%s\n" % (title, barcode, new_qual_R2))
 
